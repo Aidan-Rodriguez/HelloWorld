41b9eb9591b7ea9e260948536da48360920dba10
Aidan-Rodriguez <71618143+Aidan-Rodriguez@users.noreply.github.com>
2020-09-22 10:57:30 -0700

    Add files via upload

---
+++ HelloWorld/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // I guess I should mention that this was done by Aidan Rodriguez
+        System.out.println("Hello World!");
+    }
+}
